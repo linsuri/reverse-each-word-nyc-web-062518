678e4a90b97db5118c38eb6a09bcf7d5f21135a4
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-18 16:40:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect {|word| word.reverse}
-  array.join(" ")
+  new_array = array.collect {|word| word.reverse}
+  new_array.join(" ")
 end  
